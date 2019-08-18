7c763cc180b301f31b841fe243c2443c6f6ad0cb
Nevlaa <shorsburgh19@gmail.com>
2019-08-17 20:53:44 -0600

    Adding completed file for registration form

---
+++ X06Reservation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="X06Reservation.Global" Language="C#" %>
---
+++ X06Reservation/Request.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace X06Reservation
+{
+    public partial class Request : System.Web.UI.Page
+    {
+        private string currentDate = DateTime.Today.ToShortDateString();
+        private string currentYear = DateTime.Today.Year.ToString();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                txtArrivalDate.Text = currentDate;
+                lblYear.Text = currentYear;
+                RadioKing.Checked = true;
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "Thank you for your request.<br>" +
+                         "We will get back to you within 24 hours";
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtArrivalDate.Text = currentDate;
+            txtDepartureDate.Text = "";
+            ddlNoOfPeople.SelectedIndex = -1;
+            RadioKing.Checked = true;
+            TxtboxArea.Text = "";
+            txtFirstName.Text = "";
+            txtBoxLastName.Text = "";
+            txtBoxEmail.Text = "";
+            txtBoxTelephone.Text = "";
+            ddlPreferContact.SelectedIndex = 0;
+            lblMessage.Text = "";
+        }
+    }
+}
